d6d920173b20d614837e7b9e8bf1e2b1de4dc850
fluhus <amitlavon1.spam@gmail.com>
2025-11-21 17:04:40 -0800

    Update ghostUnion to 1 and update relevant tests

--- sketching/dist_test.go
+++ sketching/dist_test.go
@@ -8,11 +8,11 @@ func TestJaccard(t *testing.T) {
 		want float64
 	}{
 		{[]uint64{1}, nil, 0},
-		{[]uint64{1}, []uint64{1}, 1},
-		{[]uint64{1}, []uint64{1, 2}, 0.5},
+		{[]uint64{1}, []uint64{1}, 1.0 / (1 + ghostUnion)},
+		{[]uint64{1}, []uint64{1, 2}, 1.0 / (2 + ghostUnion)},
 		{[]uint64{3}, []uint64{1, 2}, 0},
 		{[]uint64{1, 2, 3}, []uint64{6, 7, 8}, 0},
-		{[]uint64{1, 2, 4}, []uint64{0, 2, 3, 4, 5}, 2.0 / 6.0},
+		{[]uint64{1, 2, 4}, []uint64{0, 2, 3, 4, 5}, 2.0 / (6 + ghostUnion)},
 	}
 
 	for _, test := range tests {
