52105f9e68a90df78c16652bb4940e477952502f
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2025-12-10 10:20:10 +0000

    Get version from blini.go

--- build_release.sh
+++ build_release.sh
@@ -1,28 +1,40 @@
+#!/usr/bin/env bash
 # Compiles blini executables for all platforms.
 
-set -e
+set -euo pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 EXE=blini
-VERSION=v0.4.0
-OUTDIR=../release
-FLAGS="-ldflags=-s -X main.version=$VERSION"
+SOURCE="$SCRIPT_DIR/$EXE/blini.go"
+# Exrtact version from "blini/blini.go" where there is a line like 'var version = "0.4.1"'
+VERSION=$(sed -n 's/.*version[[:space:]]*=[[:space:]]*"\([^"]*\)".*/\1/p' "$SOURCE" | head -n 1)
 
-rm -fr $OUTDIR
-mkdir $OUTDIR
+if [[ -z "$VERSION" ]]; then
+	echo "Could not determine version from $SOURCE" >&2
+	exit 1
+fi
+
+OUTDIR="$SCRIPT_DIR/../release"
+LDFLAGS="-s -X main.version=$VERSION"
+
+rm -fr "$OUTDIR"
+mkdir -p "$OUTDIR"
+
+echo "Building blini $VERSION"
 
 # Linux
-go build "$FLAGS" -o $OUTDIR ./$EXE
-zip -j $OUTDIR/${EXE}_linux.zip $OUTDIR/$EXE
-rm $OUTDIR/$EXE
+go build -ldflags "$LDFLAGS" -o "$OUTDIR/$EXE" "$SCRIPT_DIR/$EXE"
+zip -j "$OUTDIR/${EXE}_${VERSION}_linux.zip" "$OUTDIR/$EXE"
+rm "$OUTDIR/$EXE"
 
 # Mac
-GOOS=darwin go build "$FLAGS" -o $OUTDIR ./$EXE
-zip -j $OUTDIR/${EXE}_mac.zip $OUTDIR/$EXE
-rm $OUTDIR/$EXE
+GOOS=darwin go build -ldflags "$LDFLAGS" -o "$OUTDIR/$EXE" "$SCRIPT_DIR/$EXE"
+zip -j "$OUTDIR/${EXE}_${VERSION}_mac.zip" "$OUTDIR/$EXE"
+rm "$OUTDIR/$EXE"
 
 # Windows
-GOOS=windows go build "$FLAGS" -o $OUTDIR ./$EXE
-zip -j $OUTDIR/${EXE}_win.zip $OUTDIR/$EXE.exe
-rm $OUTDIR/$EXE.exe
+GOOS=windows go build -ldflags "$LDFLAGS" -o "$OUTDIR/$EXE.exe" "$SCRIPT_DIR/$EXE"
+zip -j "$OUTDIR/${EXE}_${VERSION}_win.zip" "$OUTDIR/$EXE.exe"
+rm "$OUTDIR/$EXE.exe"
 
-ls -lh $OUTDIR
+ls -lh "$OUTDIR"
